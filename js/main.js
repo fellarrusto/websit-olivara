cd90f14b8b4864da412efad2062a4a102861bde8
Filippo Leonelli <filippo.leonelli@linksmt.it>
2024-04-19 19:46:00 +0200

    Added some files

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-const reviewComponent = new ReviewComponent({
-    imageSrc: "img/testimonial_1.jpg",
-    name: "Roberta",
-    date: "12 Oct",
-    reviewText: "Mea ad postea meliore fuisset. Timeam repudiare id eum, ex paulo dictas elaboraret sed, mel cu unum nostrud."
-});
+// const reviewComponent = new ReviewComponent({
+//     imageSrc: "img/testimonial_1.jpg",
+//     name: "Roberta",
+//     date: "12 Oct",
+//     reviewText: "Mea ad postea meliore fuisset. Timeam repudiare id eum, ex paulo dictas elaboraret sed, mel cu unum nostrud."
+// });
 
 
-reviewComponent.inject('#reviews-container');
-reviewComponent.inject('#reviews-container');
-reviewComponent.inject('#reviews-container');
-reviewComponent.inject('#reviews-container');
+// reviewComponent.inject('#reviews-container');
+// reviewComponent.inject('#reviews-container');
+// reviewComponent.inject('#reviews-container');
+// reviewComponent.inject('#reviews-container');
